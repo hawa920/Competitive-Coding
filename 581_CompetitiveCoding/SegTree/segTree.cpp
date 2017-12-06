e06664400acf2e3da2bdec36d776b398e0a16cc8
Howard19960920 <hower.song@gmail.com>
2017-12-06 11:56:03 +0800

    segtree test template

--- 581_CompetitiveCoding/SegTree/segTree.cpp
+++ 581_CompetitiveCoding/SegTree/segTree.cpp
@@ -17,7 +17,7 @@ struct segNode
 };
 
 // 宣告四倍大小的陣列表示一棵平衡樹
-segNode segTree[MAX << 2];
+segNode segTree[MAX << 2]; // 最好 << 3 比較保險
 
 
 // 建立線段樹
@@ -26,7 +26,7 @@ void build(int l, int r, int idx)
   segTree[idx].l = l;
   segTree[idx].r = r;
 
-  if(l == r)
+  if(l == r) // 如果是左閉右開 if(r-1 == 1)
   {
     // 這裡 arr 是用來初始化的資料, 根據需求做調整
     segTree[idx].maxi = arr[l]; /*= arr[r]*/
@@ -35,18 +35,18 @@ void build(int l, int r, int idx)
 
   int mid = (l + r) / 2;
   build(l, mid, idx << 1);
-  build(mid + 1, r, idx << 1 | 1);
+  build(mid + 1, r, idx << 1 | 1); // 如果是左閉右開 build(mid, r, idx<<1|1);
 
   // 假設要找最大值, 根據需求做調整
   segTree[idx].maxi = max(segTree[idx<<1].maxi, segTree[idx<<1|1].maxi);
 }
 
-void query(int l, int r, int idx)
+void Query(int l, int r, int idx)
 {
   // l 與 r 是要查詢的區間
 
   // 判斷當下區間是否為無效區間
-  if(l > segTree[idx].r or r < segTree[idx].l)
+  if(l > segTree[idx].r or r < segTree[idx].l) // 如果是左閉右開 seg[idx].r <= l
     return;
 
   // 判斷是否為有效區間
@@ -83,7 +83,30 @@ void spUpdate(int idx, int ofst, int val)
   spUpdate(idx<<1|1, ofst, val);
 }
 
+void rgUpdate(int l, int r, int val, int idx)
+{
+  // 假設左閉右開
+  if(seg[idx].l > r or seg[idx].r <= l)
+    return;
+  if(seg[idx].l == l and seg[idx].r ==r)
+  {
+    seg[idx].?? += val;
+    lazyup(idx);
+    return;
+  }
+  int m = (seg[idx].l + seg[idx].r) / 2;
+  if(m > r)
+    rgupdate(l, r, val, idx<<1);
+  else if(m <= l)
+    rgupdate(l, r, val, idx<<1|1);
+  else
+  {
+    update(l, m, val, idx<<1);
+    update(m, r, val, idx<<1|1);
+  }
+  lazyup(idx);
 
+}
 
 int main()
 {
